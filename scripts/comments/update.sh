d62606022088ab9a2bcb24a6353ea62196c56337
slammyde7113 <dyers1@wit.edu>
2017-07-18 15:53:09 -0400

    Update Curls For Comments/Articles

--- scripts/comments/update.sh
+++ scripts/comments/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/articles"
+URL_PATH="/comments"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
@@ -9,8 +9,8 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
-    "article": {
-      "title": "Updated Title"
+    "comment": {
+      "title": "Updated Comment"
     }
   }'
 
